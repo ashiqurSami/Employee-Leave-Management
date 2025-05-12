5369af2fdd7ad04cc0c9dd95e5b78be064d1b73b
ashiqur_sami <ashiqur.rahman@bjitgroup.com>
2025-05-12 13:44:41 +0600

    feat : add login, logout and register functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from .forms import EmployeeRegistrationForm
+from .models import Employee
 
-# Create your views here.
+def register_view(request):
+    if request.method=="POST":
+        form= EmployeeRegistrationForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            Employee.objects.create()
+            login(request,user)
+            return redirect('login')
+    else:
+        form=EmployeeRegistrationForm()
+    return render(request,'accounts/register.html',{'form':form})
--- lv_mgt/urls.py
+++ lv_mgt/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include('accounts.urls')), #login, logout, Profile
+    path('',include('accounts.urls')), #login, logout, Profile
     # path('leave/',include('leave.urls')),
 ]
 
