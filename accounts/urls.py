5233149ca34ca1c105df99b8ab672fcbfd844d67
ashiqurSami <samitaseen@gmail.com>
2025-05-15 00:21:59 +0600

    feat :  add css

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('register/',views.register_view, name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'),name='login'),
+    path('',auth_views.LoginView.as_view(template_name='accounts/login.html'),name='home'),
     path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
 ]
